103a1f556295d3f84e367e181eb54de920ca052c
Mohit Agarwal <mohitagarwal@Mohits-Air.local>
2023-07-13 14:04:55 +0530

    removed python code

--- app/embedchain/loaders/local_text.py
+++
@@ -1,9 +0,0 @@
-class LocalTextLoader:
-    def load_data(self, content):
-        meta_data = {"url": "local", "text": ""}
-        return [
-            {
-                "content": content,
-                "meta_data": meta_data,
-            }
-        ]
