ca380f2b92a1926b77a78ef0c46ed30984fd325f
Mohit Agarwal <mohitagarwal@Mohits-Air.local>
2023-07-29 18:30:07 +0530

    supabse insert datasources

--- frontend/app/api/datasources/insert/route.ts
+++ frontend/app/api/datasources/insert/route.ts
@@ -1,16 +1,42 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { randomUUID } from "crypto";
-import { cookies } from "next/dist/client/components/headers";
+import { Database } from "@/types/supabase";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { PostgrestSingleResponse } from "@supabase/supabase-js";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function AddDataSource(url: string, data_type: string) {
-  const supabase = createServerComponentClient({ cookies });
+export async function POST(req: NextRequest) {
+  const supabase = createRouteHandlerClient<Database>({ cookies });
+  const req_params = await req.json();
 
-  const { error } = await supabase.from("datasources").insert({
-    type: data_type,
-    status: "queued",
+  try {
+    // Fetch data from the source table
+    const { data: dataStores, error: sourceError } = await supabase
+      .from("datastores")
+      .select("id")
+      .eq("name", req_params.datastore)
+      .eq("organization_id", req_params.organization);
 
-    meta: { url: url, characters: 100 },
-  });
+    if (!dataStores) {
+      throw new Error("datastore: not found");
+    }
 
-  return error;
+    // Insert data into the destination table
+    const { error: insertError, data } = await supabase
+      .from("datasources")
+      .insert({
+        type: req_params.file_type,
+        datastore_id: dataStores[0].id,
+        meta: { url: req_params.url, characters: 100 },
+        organization: req_params.organization,
+      });
+
+    if (insertError) {
+      throw insertError;
+    }
+
+    return NextResponse.json({ data });
+  } catch (error: any) {
+    console.log("error", error);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
